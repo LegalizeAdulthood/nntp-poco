5778b8aa55aee00366c3d16647941c2877b6ddf2
Richard <legalize@xmission.com>
2022-05-07 18:08:15 -0600

    Select newsgroup

--- nntp/news-reader/main.cpp
+++ nntp/news-reader/main.cpp
@@ -2,6 +2,7 @@
 
 #include "NNTPClientSession.h"
 #include "Poco/Net/MailMessage.h"
+#include "Poco/NumberParser.h"
 
 namespace {
 
@@ -9,23 +10,61 @@ class NewsReader
 {
 public:
     NewsReader(const std::string &server)
-        : session(server)
+        : m_session(server)
     {
-        session.open();
+        m_session.open();
+        m_groups = m_session.listNewsGroups("gmane.comp.*.boost.*");
     }
 
+    bool selectGroup();
+    bool selectArticle() { return true; }
+
 private:
-    Poco::Net::NNTPClientSession session;
-    std::vector<std::string> newsgroups;
+    Poco::Net::NNTPClientSession m_session;
+    std::vector<std::string> m_groups;
+    std::string m_currentGroup;
 };
 
+bool NewsReader::selectGroup()
+{
+    int group;
+    do
+    {
+        int i = 1;
+        for (const std::string &newsgroup : m_groups)
+        {
+            std::cout << i << ' ' << newsgroup << '\n';
+            ++i;
+        }
+        std::cout << "q Quit\n";
+        std::string cmd;
+        std::getline(std::cin, cmd);
+        if (cmd == "q")
+            return true;
+
+        group = Poco::NumberParser::parse(cmd);
+    }
+    while (group < 1 || group > static_cast<int>(m_groups.size()));
+    m_currentGroup = m_groups[group - 1];
+    m_session.selectNewsGroup(m_currentGroup);
+
+    return false;
 }
+
+} // namespace
 
 int main(int argc, char **argv)
 {
     try
     {
         NewsReader reader("news.gmane.io");
+
+        while (reader.selectGroup())
+        {
+            while (reader.selectArticle())
+            {
+            }
+        }
     }
     catch (const Poco::Exception &bang)
     {
